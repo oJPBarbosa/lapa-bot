50690a460b7c3f39f4add7e4ccee6a5d9cc517cb
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-17 14:32:08 -0300

    chore: remove command category

--- src/interfaces/ICommand.ts
+++ src/interfaces/ICommand.ts
@@ -2,7 +2,6 @@ import { CommandInteraction } from 'discord.js';
 
 export interface ICommand {
   name: string;
-  category: string;
   description: string;
   run(interaction: CommandInteraction): Promise<void>;
 }
