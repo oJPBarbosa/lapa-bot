e4d711c4a59b9bdec2467a09aa27d0738b20fc86
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-18 12:01:18 -0300

    feat(bot): add bot activity

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -5,6 +5,7 @@ export = {
   once: true,
   async execute(client: IClient, commands: any[]): Promise<void> {
     await client.application.commands.set(commands);
+    client.user.setActivity('o Lapa!', { type: 'WATCHING' });
     console.log(client.user.username + ' is up and running!');
   }
 };
