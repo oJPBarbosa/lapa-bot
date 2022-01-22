4fc791700acd7ee2977cfe71c5f7486d22cc6466
oJPBarbosa <joaopedrofbarbosa@protonmail.com>
2022-01-22 11:59:27 -0300

    refactor(api): from build to dist

--- src/handler/index.ts
+++ src/handler/index.ts
@@ -5,7 +5,7 @@ import { CommandT } from '../interfaces/Command';
 import { EventT } from '../interfaces/Event';
 
 export const handle: Function = async (client: IClient): Promise<void> => {
-  const dir: string = process.env.NODE_ENV === 'production' ? 'build' : 'src';
+  const dir: string = process.env.NODE_ENV === 'production' ? 'dist' : 'src';
 
   const commands: any[] = [];
   readdirSync(join(process.cwd(), dir, 'commands')).forEach(
@@ -17,14 +17,14 @@ export const handle: Function = async (client: IClient): Promise<void> => {
             dir,
             'commands',
             category,
-            file
+            file,
           )).default;
 
           client.commands.set(command.data.name, command);
           commands.push(command.data.toJSON());
-        }
+        },
       );
-    }
+    },
   );
 
   readdirSync(join(process.cwd(), dir, 'events')).forEach((file: string) => {
@@ -32,11 +32,11 @@ export const handle: Function = async (client: IClient): Promise<void> => {
 
     if (event.once) {
       client.once(event.name, (...args: [any]) =>
-        event.execute(...args, commands)
+        event.execute(...args, commands),
       );
     } else {
       client.on(event.name, (...args: [any]) =>
-        event.execute(...args, client.commands)
+        event.execute(...args, client.commands),
       );
     }
   });
